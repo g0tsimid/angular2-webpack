f72218b8aa4288314d136eafa29f1082b781ea96
Stephen Tsimidis <stephen.tsimidis@gmail.com>
2016-08-02 13:05:38 -0400

    Add basic router capability.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent { }
